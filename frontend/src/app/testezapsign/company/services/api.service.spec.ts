5c543d3b5510bc13acb602899e06ffbc2fbebd0b
Carla <carladmaximo@gmail.com>
2022-05-09 13:08:37 -0300

    testing endpoint GET of all services

--- frontend/src/app/testezapsign/company/services/api.service.spec.ts
+++ frontend/src/app/testezapsign/company/services/api.service.spec.ts
@@ -1,16 +1,31 @@
+import { HttpClient } from '@angular/common/http';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
 
 import { ApiService } from './api.service';
 
-describe('ApiService', () => {
+fdescribe('ApiService', () => {
   let service: ApiService;
+  let http: HttpClient;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [
+        HttpClientTestingModule
+      ]
+    });
+
     service = TestBed.inject(ApiService);
+    http = TestBed.inject(HttpClient);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+  
+  it('should call GET with correct endpoint', () => {
+    const spy = spyOn(http, 'get').and.callThrough();
+    service.getAllCompanies();
+    expect(spy).toHaveBeenCalled();
+  })
 });
--- frontend/src/app/testezapsign/user/services/api.service.spec.ts
+++ frontend/src/app/testezapsign/user/services/api.service.spec.ts
@@ -1,7 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
-import { of } from 'rxjs';
 
 import { ApiService } from './api.service';
 
@@ -9,30 +8,13 @@ fdescribe('ApiService', () => {
   let service: ApiService;
   let http: HttpClient;
 
-  // const httpStub = {
-  //   get: () => of([
-  //     {
-  //       "id_user": 20,
-  //       "email": "fernanda@pereira.com",
-  //       "email_verified": false,
-  //       "password": "aaaaa",
-  //       "last_password_redefinition_at": "2022-05-09T14:39:55.721040Z",
-  //       "created_at": "2022-05-09T14:39:55.721188Z",
-  //       "last_updated_at": "2022-05-09T14:39:55.721283Z"
-  //     }
-  //   ])
-  // }
-
   beforeEach(() => {
     TestBed.configureTestingModule({
-      // providers: [{
-      //     provide: HttpClient,
-      //     useValue: httpStub,
-      //   }]
       imports: [
         HttpClientTestingModule
       ]
     });
+    
     service = TestBed.inject(ApiService);
     http = TestBed.inject(HttpClient);
   });
